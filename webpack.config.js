30b3bab7f50c78de55a5a800273d2aad60070461
liling0726 <1329301856@qq.com>
2018-11-21 15:34:24 +0800

    判断环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin') // 分离js和css
-
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 console.log(__dirname)
-module.exports = {
-  mode: 'development',
+module.exports = (env, args) => ({
+  devtool: args.mode === 'development' ? 'source-maps' : 'eval',
   entry: {
     index: './src/index.js'
   },
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name].js'
+  },
+  optimization: {
+    minimize: false,
+    // 使用 args 来获取 mode 参数的值
+    minimizer: args.mode === 'production' ? [
+      new UglifyJsPlugin({ 
+        parallel:true,
+        cache:true,
+        uglifyOptions:{
+          compress: true,
+        }, 
+        sourceMap: true,
+       }), 
+      // 仅在我们要自定义压缩配置时才需要这么做
+      // mode 为 production 时 webpack 会默认使用压缩 JS 的 plugin
+    ] : [],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -26,10 +42,10 @@ module.exports = {
       {
         test: /\.(svg|png|jpg|gif)$/,
         use: [{
-          loader:'file-loader',
-          options:{
-            name:'[name].[ext]',
-            outputPath:'images/'
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
           }
         }]
       },
@@ -41,12 +57,12 @@ module.exports = {
           path.join(__dirname, 'src')
         ],
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',// 用什么loader提取css文件
+          fallback: 'style-loader', // 用什么loader提取css文件
           use: [ // 指定什么样的loader去编译文件
             'css-loader',
             'postcss-loader',
             'less-loader',
-          ]
+          ],
         }),
       },
       {
@@ -59,4 +75,4 @@ module.exports = {
       },
     ]
   }
-}
+})
